fb732918e49027f4e720bd009ba657434feb1f8e
Juannarvaez21 <juancarlosorellano2@gmail.com>
2024-04-21 12:08:23 -0500

    NavBar actualizada

--- americapp/lib/DirectPage.dart
+++ americapp/lib/DirectPage.dart
@@ -1,10 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class DirectPage extends StatelessWidget {
+class DirectPage extends StatefulWidget {
   const DirectPage({super.key});
+
+  @override
+  State<DirectPage> createState() => _DirectPageState();
+}
+
+class _DirectPageState extends State<DirectPage> {
+  YoutubePlayerController? _controller;
+  String _videoId = 'qRnzeJfuQ6c'; // Default video ID
+  String _title = '';
+  String _desc = '';
+  bool _isLoading = true; // Loading indicator
+  bool _isPlaying = false;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance.collection('notes').doc('doc1').get().then((doc) {
+      
+      if (doc.exists) {
+        _videoId = doc['ID'];
+        _title = doc['Title'];
+        _desc = doc['Description'];
+        if (doc.exists) {
+          _controller = YoutubePlayerController(
+            initialVideoId: _videoId,
+            flags: const YoutubePlayerFlags(
+              autoPlay: false,
+              mute: false,
+            ),
+          )..addListener(() {
+              if (mounted) {
+                setState(() {});
+              }
+            });
+          print('Controller initialized with video ID: $_videoId');
+        } else {
+          print('Invalid video ID: $_videoId');
+        }
+      } else {
+        print('Document does not exist');
+      }
+      setState(() {
+        _isLoading = false;
+      });
+    }).catchError((error) {
+      print('Error retrieving document: $error');
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+  @override
+  void dispose(){
+    _controller?.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -17,78 +76,61 @@ class DirectPage extends StatelessWidget {
             title: SizedBox(
               width: 100,
               height: 100,
-              child: Image.network(
+              child: Image.asset(
                 'assets/logo.png',
                 fit: BoxFit.cover,
               ),
             ),
           ),
           body: Container(
-
             color: Colors.blue,
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(top: 20),
+                    padding: EdgeInsets.only(top: 0),
 
                     child: Column(
                       children: [
-                        SizedBox(
-                          width: 400,
-                          child: Text(
-                            'Explorando el Mundo del Postcat: Naturaleza y Tecnología en el Después',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.lato(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ),
-
-                        Container(
-                          width: 410,
-                          height: 200,
-                          color:Colors.black,
-                        ),
-
-
-                        Container(
-                          width: 410,
-                          height: 120,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(20),
-                                bottomRight: Radius.circular(20),
-                              ),
-                              color: Colors.white,
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.25),
-                                  spreadRadius: 0,
-                                  blurRadius: 2,
-                                  offset: Offset(0, 2),
+                         if (_controller != null)
+                                Container(
+                                  child: YoutubePlayer(
+                                    controller: _controller!,
+                                    showVideoProgressIndicator: true,
+                                  ),
+                                    width: double.infinity,
+                                  height: 200,
+                                  color: Colors.black12,
                                 )
-                              ]
+                              else
+                                const CircularProgressIndicator(),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            //Titulo
+                          Container(
+                            width: 390,
+                            child: Text(
+                                                    _title?? '',
+                                                    style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold
+                            )
+                                                    ),
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Descripcion',
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.lato(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              SizedBox(height: 10,),
-                              Container(
-                                  width: 390,
-                                  child: Text("e estas criaturasdoica llena de curiosidades y sorpresas. ¡Prepárate para adentrarte en un mundo donde la naturaleza y la tecnología se entrelazan de manera inesperada!"         , textAlign: TextAlign.justify,))
-                            ],
-                          ),
+                            SizedBox(height: 10,),
+                            Container(
+                                width: 390,
+                                child:Text(
+                            _desc?? '',
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.normal
+                              )
+                            ),
+                            )
+                          ],
                         )
                       ],
                     ),
--- americapp/lib/main.dart
+++ americapp/lib/main.dart
@@ -1,139 +1,65 @@
 import 'package:americastereo/DirectPage.dart';
 import 'package:americastereo/RadioPage.dart';
+import 'package:americastereo/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash(
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _page = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-     home:Scaffold(
-       body: Container(
-           color:Colors.blue,
-           child: Navbar()),
-     ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.blue,
+          animationDuration: Duration(milliseconds: 300),
+          items: const <Widget>[
+            Icon(Icons.tv),
+            Icon(Icons.radio)
+          ],
+          onTap: (index) {
+            setState(() {
+              _page = index;
+            });
+          }
+        ),
+        body: _getBody(_page),
+      ),
     );
   }
-}
-class Navbar extends StatefulWidget {
-  const Navbar({super.key});
-
   @override
-  State<Navbar> createState() => _NavbarState();
-}
-
-class _NavbarState extends State<Navbar> with SingleTickerProviderStateMixin{
-  late TabController tabController;
-  void initState(){
-    tabController=TabController(length: 2, vsync: this);
-    super.initState();
-  }
-  void dispose(){
-    tabController.dispose();
+  void dispose() {
+  
+    // TODO: implement dispose
     super.dispose();
   }
-  @override
-  Widget build(BuildContext context) {
-      return Column(
-        children: [
-          Expanded(
-            child: TabBarView(
-              controller: tabController,
-              children: [
-                DirectPage(),
-                AnimatedLogo(),
-              ],
-            ),),
-          Container(
-            width: 400,
-            height: 80,
-            color: Colors.blue,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    width: 280,
-                    height: 65,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(100.0), // Adjust the values as needed
-                        topRight: Radius.circular(100.0), // Adjust the values as needed
-                      ),
-                      color: Colors.white,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
 
-                          Container(
-                            width: 162,
-                            height: 44,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(5),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.shade600,
-                                  spreadRadius: 1,
-                                  blurRadius: 5,
-                                  offset: const Offset(0,3 ),
-                                ),
-                              ],
-                            ),
-                            child:Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                TabBar(
-                                  unselectedLabelColor: Colors.white,//color cuando no esta seleccionado
-                                  labelColor: Colors.blue,//cuando este seleccioando
-                                  indicatorWeight: 1,
-                                  indicator: BoxDecoration(
-                                      color:Colors.white,
-                                      borderRadius: BorderRadius.circular(100)
-                                  ),
-                                  controller: tabController,
-                                  tabs: [
-                                    Container(
-                                      width: 40,
-                                      height: 40,
-                                      child: Icon(
-                                        Icons.tv,
-                                        size: 25,
-                                      ),
-                                    ),
-                                    Container(
-                                      width: 40,
-                                      height: 40,
-                                      child: Icon(
-                                        Icons.radio,
-                                        size: 25,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      );
+  Widget _getBody(int index) {
+    switch (index) {
+      case 0:
+        return DirectPage();
+      case 1:
+        return AnimatedLogo();
+      default:
+        return Container();
+    }
   }
 }
